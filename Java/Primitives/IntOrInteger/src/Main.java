eb1e89a68e2ad7530d3a48078bc5f2cbed2030b4
Anna Dmitrieva <122342446+AnnaAsp@users.noreply.github.com>
2023-01-26 00:21:19 +0200

    Update Main.java

--- Java/Primitives/IntOrInteger/src/Main.java
+++ Java/Primitives/IntOrInteger/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Container container = new Container();
-        container.addCount(5672);
-        System.out.println(container.getCount());
 
 
         for (char i = 'Ё'; i <= 'ё'; i++) {
