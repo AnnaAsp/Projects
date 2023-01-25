8edfafd4bad4abecabd966a6db0806e0133aa754
Anna Dmitrieva <122342446+AnnaAsp@users.noreply.github.com>
2023-01-26 00:32:46 +0200

    Update Main.java

--- Java/SQLAndHibernate/Homework_1/src/main/java/Main.java
+++ Java/SQLAndHibernate/Homework_1/src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/skillbox";
         String user = "root";
-        String pass = "anjutaasp";
+        String pass = "*********";
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
